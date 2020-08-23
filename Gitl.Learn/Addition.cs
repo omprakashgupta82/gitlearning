29f5681c09f4556d831ae3d974267cf18f8e17e1
Om Prakash <omprakash.gupta@outlook.com>
2020-08-23 14:54:37 +0530

    change the Concat function return type from int to string

--- Gitl.Learn/Addition.cs
+++ Gitl.Learn/Addition.cs
@@ -8,7 +8,7 @@ namespace Gitl.Learn
 {
     public class Addition
     {
-        private int Concat(string a, string b)
+        private string Concat(string a, string b)
         {
             return a + b;
         }
